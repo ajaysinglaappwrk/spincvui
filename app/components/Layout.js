ac985a6b73536ee8b0198319eb671911a3492d34
Ajay Singla <ajay.singla@appwrk.com>
2020-11-18 13:54:43 +0530

    Static content placement changes

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -6,19 +6,24 @@ import Footer from '../components/footer/footer'
 const Layout = ({ children }) => (
   <div>
     <Head>
-      <link rel='stylesheet' href='/static/assets/css/bootstrap.min.css'></link>
-      <link rel='stylesheet' href='/static/assets/plugin-css/fancybox.min.css'></link>
-      <link rel='stylesheet' href='/static/assets/css/font-awesome.min.css'></link>
-      <link rel='stylesheet' href='/static/assets/css/flaticon.min.css'></link>
-      <link rel='stylesheet' href='/static/assets/plugin-css/plugin.min.css'></link>
-      <link rel='stylesheet' href='/static/assets/css/color.min.css'></link>
-      <link rel='stylesheet' href='/static/assets/css/style.min.css'></link>
-      <link rel='stylesheet' href='/static/assets/css/common.min.css'></link>
-      <link rel='stylesheet' href='/static/assets/css/responsive.min.css'></link>
-      <link rel='stylesheet' href='/static/assets/css/slick.min.css'></link>
-      <link rel='stylesheet' href='/static/assets/css/slick-theme.min.css'></link>
-      <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/react-toastify@5.5.0/dist/ReactToastify.css'></link>
-      
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>spincv.com | Home</title>
+      <link rel="icon" href="/static/favicon.ico" />
+      <link href="" rel="stylesheet" />
+      <link rel="apple-touch-icon" sizes="57x57" href="/static/assets/images/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/static/assets/images/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/static/assets/images/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/static/assets/images/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/static/assets/images/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/static/assets/images/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/static/assets/images/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/static/assets/images/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/assets/images/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/static/assets/images/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/assets/images/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/static/assets/images/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/assets/images/favicon-16x16.png" />
     </Head>
     <Header></Header>
     <div>
--- pages/_document.js
+++ pages/_document.js
@@ -17,9 +17,33 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-87124676-2"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-87124676-2');
+`}}
+          />
+         
+          <link rel='stylesheet' href='/static/assets/css/bootstrap.min.css'></link>
+          <link rel='stylesheet' href='/static/assets/plugin-css/fancybox.min.css'></link>
+          <link rel='stylesheet' href='/static/assets/css/font-awesome.min.css'></link>
+          <link rel='stylesheet' href='/static/assets/css/flaticon.min.css'></link>
+          <link rel='stylesheet' href='/static/assets/plugin-css/plugin.min.css'></link>
+          <link rel='stylesheet' href='/static/assets/css/color.min.css'></link>
+          <link rel='stylesheet' href='/static/assets/css/style.min.css'></link>
+          <link rel='stylesheet' href='/static/assets/css/common.min.css'></link>
+          <link rel='stylesheet' href='/static/assets/css/responsive.min.css'></link>
+          <link rel='stylesheet' href='/static/assets/css/slick.min.css'></link>
+          <link rel='stylesheet' href='/static/assets/css/slick-theme.min.css'></link>
+          <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/react-toastify@5.5.0/dist/ReactToastify.css'></link>
         </Head>
         <body>
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGKR5LW" height="0" width="0"
+            style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
           <Main />
           <NextScript />
         </body>
