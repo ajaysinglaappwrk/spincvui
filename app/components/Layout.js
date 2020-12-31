c125b321ceacb99d659e0d2b42e5dbe5fd5ebee3
Ajay Singla <ajay.singla@appwrk.com>
2020-12-31 11:36:29 +0530

    Icons used in layout js moved to azure cdn

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -9,21 +9,21 @@ const Layout = ({ children }) => (
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>spincv.com | Home</title>
-      <link rel="icon" href="/static/favicon.ico" />
+      <link rel="icon" href="https://my-cdn.azureedge.net/cdn/images/favicon.ico" />
       <link href="" rel="stylesheet" />
-      <link rel="apple-touch-icon" sizes="57x57" href="/static/assets/images/apple-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="60x60" href="/static/assets/images/apple-icon-60x60.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/static/assets/images/apple-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/static/assets/images/apple-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/static/assets/images/apple-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/static/assets/images/apple-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/static/assets/images/apple-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/static/assets/images/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/static/assets/images/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/static/assets/images/android-icon-192x192.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/assets/images/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/static/assets/images/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/assets/images/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="57x57" href="https://my-cdn.azureedge.net/cdn/images/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="https://my-cdn.azureedge.net/cdn/images/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="https://my-cdn.azureedge.net/cdn/images/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="https://my-cdn.azureedge.net/cdn/images/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="https://my-cdn.azureedge.net/cdn/images/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="https://my-cdn.azureedge.net/cdn/images/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="https://my-cdn.azureedge.net/cdn/images/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="https://my-cdn.azureedge.net/cdn/images/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="https://my-cdn.azureedge.net/cdn/images/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="https://my-cdn.azureedge.net/cdn/images/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="https://my-cdn.azureedge.net/cdn/images/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="https://my-cdn.azureedge.net/cdn/images/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="https://my-cdn.azureedge.net/cdn/images/favicon-16x16.png" />
       
       <meta property="og:description"
         content="Le seul site d’emploi qui vous permet vraiment de découvrir une entreprise.  Vidéos, photos, visite 3D, drone, direct, et bien plus.  En 2020, ne vous fiez plus seulement aux offres écrites.  Totalement gratuit !" />
